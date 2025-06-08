c7fa363281bac0edb132839f50de886960d22170
Anh Tran <anh.vt2@gmail.com>
2025-06-07 20:48:00 -0400

    add 23-merge-k-sorted-list.py

---
+++ 23-merge-k-sorted-list.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Idea: 
+        # (1) Convert linked-lists to simple list
+        # (2) Sort
+        # (3) Convert simple list to a linked list
+        # Flatten linked-lists to simple list
+        nodes = []
+        for l in lists:
+            while l:
+                nodes.append(l.val)
+                l = l.next
+        # Sort
+        nodes.sort()
+        # Convert simple lists to a linked list
+        head = root = ListNode()
+        for node in nodes:
+            root.next = ListNode(node)
+            root = root.next
+        return head.next
