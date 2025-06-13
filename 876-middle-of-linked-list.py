25701a9f795c76449fca880769f0a230f8be7af8
Anh Tran <anh.vt2@gmail.com>
2025-06-13 19:58:45 -0400

    add 876-middle-of-linked-list.py

---
+++ 876-middle-of-linked-list.py
@@ -0,0 +1,48 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Helper function to create a linked list from a list of list_numbers
+def build_list(list_numbers):
+    dummy = ListNode()
+    current = dummy
+    for n in list_numbers:
+        current.next = ListNode(n)
+        current = current.next
+    return dummy.next
+
+# Helper function to print a linked list as a list
+def print_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    print(result)
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+# For odd-length lists, slow lands at the true middle.
+# For even-length lists, slow lands at the second middle (which is what the problem wants).
+
+test_lists = [1,2,3,4,5]
+test_linked_list = build_list(test_lists)
+sol = Solution()
+result =  sol.middleNode(test_linked_list)
+print(result)
