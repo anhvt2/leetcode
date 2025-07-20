8bb36ee483cdc20fbafe1eaee131d2b17b5a8e8b
Anh Tran <anh.vt2@gmail.com>
2025-07-20 10:09:13 -0400

    add 176

--- sql/1211-queries-quality-and-percentage.sql
+++ sql/1211-queries-quality-and-percentage.sql
@@ -1,26 +1,7 @@
--- SELECT
---     query_name,
---     ROUND(AVG(rating/position), 2) AS quality,
---     ROUND(AVG(
---         CASE WHEN rating < 3 
---         THEN 1 ELSE 0 
---         END) * 100.0, 2) AS poor_query_percentage
--- FROM Queries
--- GROUP BY query_name;
-
-SELECT 
-    q.query_name, 
-    round(avg(rating/position), 2) AS quality, 
-    ifnull(poor.poor_query_percentage, 0) AS poor_query_percentage
-FROM Queries q
-LEFT JOIN (
-    SELECT poor.query_name, round(count(*) / total.count * 100, 2) AS poor_query_percentage
-    FROM Queries poor 
-    JOIN (
-        SELECT query_name, count(*) AS count FROM Queries GROUP BY query_name
-    ) total
-    ON poor.query_name = total.query_name AND poor.rating < 3
-    GROUP BY poor.query_name
-) poor
-ON poor.query_name = q.query_name
-GROUP BY query_name
+SELECT
+    query_name,
+    round(avg(rating / position), 2) AS quality,
+    round(sum(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / count(*), 2)
+        AS poor_query_percentage
+FROM queries
+GROUP BY query_name;
---
+++ sql/176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+-- # Write your MySQL query statement below
+SELECT coalesce((
+    SELECT DISTINCT salary
+    FROM employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+), null) AS secondhighestsalary
