f1b9be20726e31401b2634d1d0302f24be1ad73d
Anh Tran <anh.vt2@gmail.com>
2025-07-23 11:21:11 -0400

    add 314 tree traversal py

--- sql/185-department-top-three-salaries.sql
+++ sql/185-department-top-three-salaries.sql
@@ -1,13 +1,16 @@
 -- # Write your MySQL query statement below
-SELECT 
-    d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary
+SELECT
+    d.name AS department,
+    e.name AS employee,
+    e.salary
 FROM (
-    SELECT *,
-           DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
-    FROM Employee
-) e
-JOIN Department d
-  ON e.departmentId = d.id
+    SELECT
+        *,
+        DENSE_RANK()
+            OVER (PARTITION BY departmentid ORDER BY salary DESC)
+            AS rnk
+    FROM employee
+) AS e
+INNER JOIN department AS d
+    ON e.departmentid = d.id
 WHERE e.rnk <= 3;
