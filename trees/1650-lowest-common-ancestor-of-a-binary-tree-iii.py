20ac975160b6d51ce21230fb48e6c0c37d1825bd
Anh Tran <anh.vt2@gmail.com>
2025-07-27 11:15:28 -0400

    add lowest common ancestor binary tree examples

---
+++ trees/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        a, b = p, q
+        while a != b:
+            a = a.parent if a else q
+            b = b.parent if b else p
+        return a # return b
+        # Time complexity: O(h) -- height
+        # Space complexity: O(1)
+    
