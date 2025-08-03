b7e51938ac82cfb098fdaeb3fd5c15ec3f7671f2
Anh Tran <anh.vt2@gmail.com>
2025-08-03 13:55:27 -0400

    add 160 py

---
+++ lists/160-intersection-of-two-linked-lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# O(m+n) time complexity
+# O(1) space complexity
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        """
+        If the two lists intersect, returns the first common node; otherwise None.
+        """
+        if not headA or not headB:
+            return None
+
+        a, b = headA, headB
+        while a is not b:
+            # Advance two pointers - when any reaches the end, then redirect to another linked list
+            # If there is an intersection, a and b will meet after at most two passes
+            a = a.next if a else headB
+            b = b.b.next if b else headA
+        
+        return a # or b
