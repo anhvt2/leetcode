33c0ce0727374de72dabc6ce329fba793d8cfb2f
Anh Tran <anh.vt2@gmail.com>
2025-08-04 11:03:38 -0400

    add 162

---
+++ lists/162-find-a-peak-element.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            # if slope is rising, go right
+            if nums[mid] < nums[mid+1]:
+                left = mid + 1 
+            else:
+                # slope is falling, go left
+                right = mid
+        # peak is where left == right
+        return left
+
+# class Solution:
+#     def findPeakElement(self, nums: List[int]) -> int:
+#         if len(nums) < 2:
+#             return 0
+#         elif len(nums) == 2:
+#             return 0 if nums[0] > nums[1] else 1
+#         else:
+#             for i in range(1, len(nums) - 1):
+#                 if nums[i-1] < nums[i] and nums[i] > nums[i+1]:
+#                     return i
+#             if nums[0] > nums[1]:
+#                 return 0
+#             elif nums[-1] > nums[-2]:
+#                 return len(nums)-1
