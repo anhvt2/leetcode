840f28fa8c301a352fd9623fb6c55e5103152e1c
Anh Tran <anh.vt2@gmail.com>
2025-08-03 22:19:26 -0400

    add 123

--- lists/128-longest-consecutive-consequence.py
+++ lists/128-longest-consecutive-consequence.py
@@ -14,3 +14,18 @@
 #                     tmp = [num] # reset but don't drop the current num
 #         res.append(tmp)
 #         return max([len(chunk) for chunk in res])
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums = sorted(set(nums))
+        best = curr = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                curr += 1
+            else:
+                best = max(best, curr)
+                curr = 1
+        return max(best, curr)
