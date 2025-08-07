9237519cecc3ff1a46969b61a2881c9891144991
Anh Tran <anh.vt2@gmail.com>
2025-08-07 13:11:09 -0400

    add 116

---
+++ trees/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+# Convert a tree node to a linked list
+from collections import deque
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+
+        q = deque([root])
+        while q:
+            prev = None
+            # process one level at a time
+            for _ in range(len(q)):
+                node = q.popleft()
+                if prev:
+                    prev.next = node
+                prev = node
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            # end of this level -> last node's next should already be None
+            prev.next = None
+
+        return root
