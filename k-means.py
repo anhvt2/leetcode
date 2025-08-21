1cb720ecefbe5d292f51cbb4b8d82e2949f93b24
Anh Tran <anh.vt2@gmail.com>
2025-08-21 14:43:54 -0400

    update k-means

--- k-means.py
+++ k-means.py
@@ -1,22 +1,29 @@
 import numpy as np
 
-def kmeans(X, k, max_iter=100):
-    # Randomly choose k initial centroids from data
-    centroids = X[np.random.choice(len(X), k, replace=False)]
-    
-    for _ in range(max_iter):
-        # Assign points to nearest centroid
-        labels = np.argmin(((X[:, None] - centroids)**2).sum(axis=2), axis=1)
-        # Compute new centroids
-        new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(k)])
-        # Stop if converged
-        if np.allclose(centroids, new_centroids): break
-        centroids = new_centroids
-    
-    return centroids, labels
+class KMeans:
+    def __init__(self, k, max_iter=100, tol=1e-4, random_state=None):
+        self.k = k
+        self.max_iter = max_iter
+        self.tol = tol
+        self.random_state = random_state
+        self.centroids = None
 
-X = np.random.rand(100, 2)  # 100 2D points
-centroids, labels = kmeans(X, k=3)
-print(X)
-print(centroids)
-print(labels)
+    def fit(self, X):
+        rng = np.random.default_rng(self.random_state)
+        # Randomly initialize centroids
+        self.centroids = X[rng.choice(len(X), self.k, replace=False)]
+
+        for _ in range(self.max_iter):
+            # Compute distances and assign clusters
+            labels = self.predict(X)
+            # Update centroids
+            new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.k)])
+            # Check convergence
+            if np.allclose(self.centroids, new_centroids, atol=self.tol):
+                break
+            self.centroids = new_centroids
+
+    def predict(self, X):
+        # Assign each point to the nearest centroid
+        dists = np.linalg.norm(X[:, None] - self.centroids, axis=2)
+        return np.argmin(dists, axis=1)
