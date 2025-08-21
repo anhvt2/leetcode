651fdb43fe467420e1d54bf90f6fe9518fd82b2a
Anh Tran <anh.vt2@gmail.com>
2025-08-21 16:42:24 -0400

    add 1587

---
+++ database/1587-bank-account-summary-ii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    u.name,
+    sum(t.amount) AS balance
+FROM users AS u
+INNER JOIN transactions AS t ON u.account = t.account
+GROUP BY u.account, u.name
+HAVING sum(t.amount) > 10000;
