770cf9e471a63f8d779b0946241c20635b0bdb55
Anh Tran <anh.vt2@gmail.com>
2025-08-22 13:13:25 -0400

    add second solution to 1084

--- database/1084-sales-analysis-iii.sql
+++ database/1084-sales-analysis-iii.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# First solution
 SELECT
     s.product_id,
     p.product_name
@@ -8,3 +9,18 @@ GROUP BY product_id
 HAVING
     min(s.sale_date) >= '2019-01-01'
     AND max(s.sale_date) <= '2019-03-31';
+
+-- # Second solution
+-- SELECT DISTINCT
+--     s.product_id,
+--     p.product_name
+-- FROM Sales s
+-- JOIN Product p 
+--     ON s.product_id = p.product_id
+-- WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+--   AND NOT EXISTS (
+--       SELECT NULL           # SELECT 1, NULL, or * all works
+--       FROM Sales s2
+--       WHERE s2.product_id = s.product_id
+--         AND (s2.sale_date < '2019-01-01' OR s2.sale_date > '2019-03-31')
+--   );
