14669d1aa2c6ebd535020118466aedc22ebd4100
Anh Tran <anh.vt2@gmail.com>
2025-08-22 13:01:02 -0400

    add another solution for 1083

--- database/1083-sales-analysis-ii.sql
+++ database/1083-sales-analysis-ii.sql
@@ -1,8 +1,25 @@
 # Write your MySQL query statement below
-SELECT s.buyer_id
-FROM sales AS s
-LEFT JOIN product AS p ON s.product_id = p.product_id
-GROUP BY s.buyer_id
-HAVING
-    sum(p.product_name = 'S8') > 0
-    AND sum(p.product_name = 'iPhone') = 0;
+# First solution
+SELECT DISTINCT s.buyer_id
+FROM Sales s
+WHERE EXISTS (
+  SELECT seller_id
+  FROM Sales s2
+  JOIN Product p2 ON p2.product_id = s2.product_id
+  WHERE s2.buyer_id = s.buyer_id AND p2.product_name = 'S8'
+)
+AND NOT EXISTS (
+  SELECT seller_id
+  FROM Sales s3
+  JOIN Product p3 ON p3.product_id = s3.product_id
+  WHERE s3.buyer_id = s.buyer_id AND p3.product_name = 'iPhone'
+);
+
+# Second solution
+-- SELECT s.buyer_id
+-- FROM sales AS s
+-- LEFT JOIN product AS p ON s.product_id = p.product_id
+-- GROUP BY s.buyer_id
+-- HAVING
+--     sum(p.product_name = 'S8') > 0
+--     AND sum(p.product_name = 'iPhone') = 0;
