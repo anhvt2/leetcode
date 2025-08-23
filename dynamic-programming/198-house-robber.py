2aad0c66347a66f8fab47aa656ca6212ed3de335
Anh Tran <anh.vt2@gmail.com>
2025-08-23 12:44:09 -0400

    add a clean solution for 198

--- dynamic-programming/198-house-robber.py
+++ dynamic-programming/198-house-robber.py
@@ -7,19 +7,30 @@ class Solution:
         elif len(nums) == 1:
             return nums[0]
         
-        # For more than two houses, at each house i, you have two choices:
-        # 1. Rob the current house i and add its value to the maximum money robbed up to house i-2.
-        # 2. Skip the current house and take the maximum money robbed up to house i-1.
-        # Idea: dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        # Initialize the first two values of the DP array
-        prev2 = 0  # dp[i-2]
-        prev1 = nums[0]  # dp[i-1]
+        # # For more than two houses, at each house i, you have two choices:
+        # # 1. Rob the current house i and add its value to the maximum money robbed up to house i-2.
+        # # 2. Skip the current house and take the maximum money robbed up to house i-1.
+        # # Idea: dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         
-        # Iterate through the list starting from the second index
-        for i in range(1, len(nums)):
-            current = max(prev1, prev2 + nums[i])  # Max of robbing or not robbing the current house
-            prev2 = prev1  # Move prev1 to prev2
-            prev1 = current  # Move current to prev1
+        # Time complexity: O(n)
+        # Space complexity: O(1) -- very obscure: two pointers tracking previous and current indices
+        # prev2 = 0  # dp[i-2]
+        # prev1 = nums[0]  # dp[i-1]
         
-        # The result will be stored in prev1 (dp[n-1])
-        return prev1
+        # # Iterate through the list starting from the second index
+        # for i in range(1, len(nums)):
+        #     current = max(prev1, prev2 + nums[i])  # Max of robbing or not robbing the current house
+        #     prev2 = prev1  # Move prev1 to prev2
+        #     prev1 = current  # Move current to prev1
+        
+        # # The result will be stored in prev1 (dp[n-1])
+        # return prev1
+
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
