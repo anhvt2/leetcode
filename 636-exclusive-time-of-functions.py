c4282a1a7ac074d57064fe4a699b47cfcdb082b9
Anh Tran <anh.vt2@gmail.com>
2025-08-23 21:59:24 -0400

    add 636

---
+++ 636-exclusive-time-of-functions.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        res = [0] * n
+        stack = []          # stack of running function ids
+        prev = 0            # previous timestamp boundary
+
+        for log in logs:
+            fid_str, typ, t_str = log.split(':')
+            fid = int(fid_str)
+            t = int(t_str)
+
+            if typ == 'start':
+                # Charge elapsed time since prev to the function currently on CPU
+                if stack:
+                    res[stack[-1]] += t - prev
+                stack.append(fid)
+                prev = t
+            else:  # 'end'
+                # Finish current function; include this timestamp
+                f = stack.pop()
+                res[f] += t - prev + 1
+                prev = t + 1   # next slice starts after this end time
+
+        return res
