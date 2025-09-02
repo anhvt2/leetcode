a9c26335a261d1fdd18b45ca530955e0c19e77ed
Anh Tran <anh.vt2@gmail.com>
2025-09-02 17:39:36 -0400

    add 257

---
+++ trees/257-binary-tree-paths.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import List
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # ---- First Solution
+        def dfs(root: TreeNode, s: str):
+            if not root.left and not root.right:
+                res.append(s)
+            if root.left:
+                tmp_l = s + '->' + str(root.left.val)
+                dfs(root.left, tmp_l)
+            if root.right:
+                tmp_r = s + '->' + str(root.right.val)
+                dfs(root.right, tmp_r)
+        res = []
+        dfs(root, str(root.val))
+        return res
+        
+        # ---- Second Solution
+        # if not root:
+        #     return []
+
+        # paths = []
+        # def dfs(node: 'TreeNode', cur: List[str]) -> None:
+        #     # Append current node value to the path
+        #     cur.append(str(node.val))
+
+        #     # If it's a leaf, join the path and store it
+        #     if not node.left and not node.right:
+        #         paths.append("->".join(cur))
+        #     else:
+        #         # Recurse to children
+        #         if node.left:
+        #             dfs(node.left, cur)
+        #         if node.right:
+        #             dfs(node.right, cur)
+        #     # Backtrack
+        #     cur.pop()
+
+        # dfs(root, [])
+        # return paths
