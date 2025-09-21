e5943138fef5fde5d20abd34c7c0f95e1667e878
Anh Tran <anh.vt2@gmail.com>
2025-09-21 10:33:15 -0400

    add 1912

---
+++ 1912-design-movie-rental-system.py
@@ -0,0 +1,45 @@
+from sortedcontainers import SortedList
+
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        
+        self.store = defaultdict(int)
+        self.films = defaultdict(SortedList)
+        self.rental = set()
+
+        for shop, movie, price in entries:
+            self.store[(shop, movie)] = price
+            self.films[movie].add((price, shop))
+            
+
+    def search(self, movie: int) -> List[int]:
+        a = self.films[movie][:5]
+        return [x[1] for x in a]
+
+
+    def rent(self, shop: int, movie: int) -> None:
+
+        price = self.store[(shop, movie)]
+        self.films[movie].discard((price, shop))
+        self.rental.add((price, shop, movie))
+        
+
+    def drop(self, shop: int, movie: int) -> None:
+ 
+        price = self.store[(shop, movie)]
+        self.films[movie].add((price, shop))
+        self.rental.discard((price, shop, movie))
+    
+
+    def report(self) -> List[List[int]]:
+
+        report_stores = sorted(self.rental)[:5]
+        return [[s, m] for p, s, m in report_stores]
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
