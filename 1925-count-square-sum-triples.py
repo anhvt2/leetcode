7dd7f14268640e5f78931924a49eb381f5d1d78c
Anh Tran <anh.vt2@gmail.com>
2025-12-07 20:49:36 -0500

    add 1925

---
+++ 1925-count-square-sum-triples.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        total = 0
+        squares = set([i**2 for i in range(n+1)])
+
+        for x in range(1, n+1):
+            for y in range(x+1, n+1):
+                if x**2 + y**2 in squares:
+                    total += 2
+        return total
