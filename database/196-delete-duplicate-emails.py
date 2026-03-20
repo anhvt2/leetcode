9e61e214e5d428a0d3730c0bd4bda3924ea07b51
Anh Tran <anh.vt2@gmail.com>
2026-03-20 15:08:36 -0400

    add 196

---
+++ database/196-delete-duplicate-emails.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values('id', inplace=True) # sort 'id': lowest first
+    person.drop_duplicates(subset='email',
+                            keep='first',
+                            inplace=True,
+                            )
