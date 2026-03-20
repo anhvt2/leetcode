528b5abad0c9e02b8956c4e1ed52c6dc46754786
Anh Tran <anh.vt2@gmail.com>
2026-03-20 16:09:01 -0400

    add 1149 pandas

---
+++ database/pandas/1149-article-views-ii.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return (views.groupby(['viewer_id', 'view_date'])['article_id']
+            .nunique() # count distinct article
+            .reset_index()
+            .query('article_id > 1') # more than 1 article
+            [['viewer_id']].drop_duplicates()
+            .rename(columns={'viewer_id': 'id'})
+            .sort_values('id')
+        )
